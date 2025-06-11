f6b87579c45c57f3abe20e116e9c932b06f28967
Ember Shan <szixin@umich.edu>
2025-06-11 14:11:47 -0400

    set up shadcn+jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Catalogue from '@/components/Catalogue'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="app">
-      <h1>React + Vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <Catalogue />
     </div>
   )
 }
